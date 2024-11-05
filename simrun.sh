9753b1c475074502cd83cbe3a798b5c285b55ba4
Aya Mitani <ayamitani@gmail.com>
2024-11-05 16:35:24 -0500

    updated simrun.sh

--- simrun.sh
+++ simrun.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=simjob.txt
 #SBATCH --mail-type=FAIL         # send an email if job aborted
 
-module load gcc/9.3.0 r/4.1.2
+module load intel/2019u4 gcc/8.3.0 r/4.1.2
 
 N=50
 B=100
