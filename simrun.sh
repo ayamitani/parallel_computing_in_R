1be0d55ad51918991026bd2eb436090c61072f87
Aya Mitani <ayamitani@gmail.com>
2024-11-05 16:31:57 -0500

    updated simrun.sh

--- simrun.sh
+++ simrun.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1                # number of node MUST be 1
+#SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=4        # number of processes
-#SBATCH --mem-per-cpu=2048M      # memory; default unit is megabytes
 #SBATCH --time=1:00:00           # time (DD-HH:MM)
+#SBATCH --output=simjob.txt
 #SBATCH --mail-type=FAIL         # send an email if job aborted
 
 module load gcc/9.3.0 r/4.1.2
